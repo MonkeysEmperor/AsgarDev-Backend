c6f85314c5948175c92dd2d34744a4af85ab82a4
Gabriel Rocha Amorim <gabriel.rocha.amorim@usp.br>
2018-12-17 02:39:16 -0200

    Refatorando código (erro nas identações tabXspace)

--- app/app.py
+++ app/app.py
@@ -1,16 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
-#Importar cada recurso usado pela API.
+# Importar cada recurso usado pela API.
 from lista.resources.demanda_resource import DemandaResource, DemandasResource
 from lista.resources.servico_resource import ServicoResource, ServicosResource
-from lista.resources.login_resource import LoginResource 
-from lista.resources.cliente_resource import ClienteResource, ClientesResource 
+from lista.resources.login_resource import LoginResource
+from lista.resources.cliente_resource import ClienteResource, ClientesResource
 from lista.resources.fornecedor_resource import FornecedorResource, FornecedoresResource
 from lista.resources.gestor_resource import GestorResource, GestoresResource
 
 app = Flask(__name__)
-#Configurações relativas ao sqlalchemy
+# Configurações relativas ao sqlalchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
@@ -18,17 +18,21 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 app.secret_key = b'\xc4]gW\x0f\x8d\xc8\x05ocG\xf1\xb1j,{'
 
-#fim configurações relativas ao sqlalchemy
+# fim configurações relativas ao sqlalchemy
 api = Api(app)
-CORS(app,resources={r"/*": {"origins": "*"}}) #O uso do cors
-#cria as tabelas do banco de dados, caso elas não estejam criadas
+CORS(app, resources={r"/*": {"origins": "*"}})  # O uso do cors
+
+
+# cria as tabelas do banco de dados, caso elas não estejam criadas
 @app.before_first_request
 def create_tables():
-	from dao import db
-        db.init_app(app)
-        print("criar tabelas")
-	db.create_all()
-#fim criação de tabelas
+    from dao import db
+    db.init_app(app)
+    print("criar tabelas")
+    db.create_all()
+
+
+# fim criação de tabelas
 
 api.add_resource(DemandasResource, '/demandas')
 api.add_resource(DemandaResource, '/demanda', '/demanda/<int:item>')
@@ -36,14 +40,15 @@ api.add_resource(ServicosResource, '/servicos')
 api.add_resource(ServicoResource, '/servico', '/servico/<int:item>')
 
 api.add_resource(LoginResource, '/login')
-api.add_resource(ClienteResource,'/cliente','/cliente/<int:item>')
+api.add_resource(ClienteResource, '/cliente', '/cliente/<int:item>')
 api.add_resource(ClientesResource, '/clientes')
-api.add_resource(FornecedorResource,'/fornecedor','/fornecedor/<int:item>')
+api.add_resource(FornecedorResource, '/fornecedor', '/fornecedor/<int:item>')
 api.add_resource(FornecedoresResource, '/fornecedores')
-api.add_resource(GestorResource,'/gestor','/gestor/<int:item>')
+api.add_resource(GestorResource, '/gestor', '/gestor/<int:item>')
 api.add_resource(GestoresResource, '/gestores')
 
 if __name__ == '__main__':
-	from dao import db
-	db.init_app(app)
-	app.run(port=5000,debug=True)
+    from dao import db
+
+    db.init_app(app)
+    app.run(port=5000, debug=True)
